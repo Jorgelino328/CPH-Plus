c2d716a7dc50eaa8afd9a7d40e42913da724a3cd
Pablo1Gustavo <pablo1gustavo.dev@gmail.com>
2023-11-25 17:30:09 -0300

    FT: add paste model and migration.

---
+++ api/database/migrations/2023_11_25_190951_create_pastes_table.php
@@ -0,0 +1,34 @@
+<?php
+use App\Models\SyntaxHighlight;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('pastes', function (Blueprint $table)
+        {
+            $table->id();
+            $table->foreignIdFor(User::class)->constrained();
+            $table->foreignIdFor(SyntaxHighlight::class)->constrained();
+
+            $table->string('title', 50);
+            $table->string('tags')->nullable();
+            $table->text('content');
+            $table->boolean('listable')->default(true);
+            $table->string('password', 60)->nullable();
+            $table->boolean('destroy_on_open')->default(false);
+            $table->timestamp('created_at');
+
+            $table->fullText(['title', 'tags']);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('pastes');
+    }
+};
