d7fca9f306c3a0a33bd9705fdcca5e379c915b05
Pablo1Gustavo <pablo1gustavo.dev@gmail.com>
2023-11-25 18:20:03 -0300

    FT: add expiration time and syntax highlight seeders.

---
+++ api/database/seeders/ExpirationTimeSeeder.php
@@ -0,0 +1,20 @@
+<?php
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class ExpirationTimeSeeder extends Seeder
+{
+    public function run(): void
+    {
+        \App\Models\ExpirationTime::insert([
+            ['label' => 'Never'     , 'seconds' => 0                 ],
+            ['label' => '10 minutes', 'seconds' => 10  * 60          ],
+            ['label' => '1 hour'    , 'seconds' => 60  * 60          ],
+            ['label' => '1 day'     , 'seconds' => 24  * 60 * 60     ],
+            ['label' => '1 week'    , 'seconds' => 7   * 24 * 60 * 60],
+            ['label' => '1 month'   , 'seconds' => 30  * 24 * 60 * 60],
+            ['label' => '1 year'    , 'seconds' => 360 * 24 * 60 * 60],
+        ]);
+    }
+}
