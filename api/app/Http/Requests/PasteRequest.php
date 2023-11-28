39e2ae934a9f83bc92fb7d651ac8a62b1d083a49
Pablo1Gustavo <pablo1gustavo.dev@gmail.com>
2023-11-27 22:20:25 -0300

    FX: move sanitization logic of PasteRequest to the passedVali method.

--- api/app/Http/Requests/PasteRequest.php
+++ api/app/Http/Requests/PasteRequest.php
@@ -8,28 +8,28 @@ class PasteRequest extends FormRequest
 {
     use Updatable;
 
-    protected function prepareForValidation()
-    {
-        $this->merge([
-            'tags' => array_map(
-                fn ($value) => str_replace(',', ' ', $value),
-                $this->input('tags', [])
-            )
-        ]);
-    }
-
     public function rules(): array
     {
         return $this->handleUpdate([
-            'syntax_highlight_id' => 'nullable|exists:syntax_highlights',
+            'syntax_highlight_id' => 'nullable|exists:syntax_highlights,id',
             'seconds_to_expire'   => 'nullable|integer|min:600|max:31536000',
-            'title'               => 'required|string',
-            'tags'                => 'nullable|array',
+            'title'               => 'required|string|max:50',
+            'tags'                => 'nullable|array|max:10',
             'tags.*'              => 'required|distinct|string|max:25',
             'content'             => 'required|string|max:500000',
             'listable'            => 'nullable|boolean',
             'password'            => 'nullable|string|min:8',
             'destroy_on_open'     => 'nullable|boolean'
+        ]);
+    }
+
+    protected function passedValidation()
+    {
+        $this->merge([
+            'tags' => array_map(
+                fn ($value) => str_replace(',', '‚', $value),
+                $this->input('tags') ?? []
+            )
         ]);
     }
 }
