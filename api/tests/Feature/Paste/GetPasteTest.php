43e3accd0c041a27dfc97ef87d0257ec8090f436
Pablo1Gustavo <pablo1gustavo.dev@gmail.com>
2023-11-28 15:52:38 -0300

    TT: add log generation asserts to get paste tests.

--- api/tests/Feature/Paste/GetPasteTest.php
+++ api/tests/Feature/Paste/GetPasteTest.php
@@ -2,6 +2,7 @@
 namespace Tests\Feature\Paste;
 
 use App\Models\Paste;
+use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
@@ -12,7 +13,7 @@ class GetPasteTest extends TestCase
 
     // SUCCESS
 
-    public function test_get_paste_that_doesnt_have_password()
+    public function test_get_paste_that_doesnt_have_password_without_user_and_generate_log()
     {
         $paste = Paste::factory()->create(['password' => null]);
 
@@ -20,11 +21,27 @@ class GetPasteTest extends TestCase
             ->assertStatus(200);
 
         $this->assertDatabaseHas('paste_access_logs', [
-            'paste_id' => $paste->id
+            'paste_id' => $paste->id,
+            'user_id'  => null
         ]);
     }
 
-    public function test_get_paste_with_password()
+    public function test_get_paste_that_doesnt_have_password_with_user_and_generate_log()
+    {
+        $paste = Paste::factory()->create(['password' => null]);
+        $user = User::factory()->create();
+
+        $this->actingAs($user)
+            ->post('/api/pastes/' . $paste->id)
+            ->assertStatus(200);
+
+        $this->assertDatabaseHas('paste_access_logs', [
+            'paste_id' => $paste->id,
+            'user_id'  => $user->id
+        ]);
+    }
+
+    public function test_get_paste_with_password_and_generate_log()
     {
         $paste = Paste::factory()->create(['password' => Hash::make('12345678')]);
 
@@ -32,7 +49,8 @@ class GetPasteTest extends TestCase
             ->assertStatus(200);
 
         $this->assertDatabaseHas('paste_access_logs', [
-            'paste_id' => $paste->id
+            'paste_id' => $paste->id,
+            'user_id'  => null
         ]);
     }
 
